db27c826d9f1ad9fdca0dee4b3838afd7cb72e9b
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2020-11-05 03:27:39 +0100

    fix frame resize after media query

--- src/js/frame.js
+++ src/js/frame.js
@@ -1,3 +1,9 @@
 function resizeFrame(frame) {
-    frame.style.height=frame.contentWindow.document.body.scrollHeight + "px";
+    var target = frame.contentWindow.document.body;
+    var config = { attributes: true, childList: true, characterData: true };
+    var callback = function(mutationsList) {
+        frame.style.height = target.scrollHeight + "px";
+    };
+    var observer = new MutationObserver(callback);
+    observer.observe(target, config);
 }
