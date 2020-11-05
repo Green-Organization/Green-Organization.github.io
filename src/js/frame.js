2dffd1fe252b214e0499a0031bf34cfee619d383
Fabio Buracchi <45599613+buracchi@users.noreply.github.com>
2020-11-05 03:31:26 +0100

    Update frame.js

--- src/js/frame.js
+++ src/js/frame.js
@@ -2,8 +2,13 @@ function resizeFrame(frame) {
     var target = frame.contentWindow.document.body;
     var config = { attributes: true, childList: true, characterData: true };
     var callback = function(mutationsList) {
-        frame.style.height = target.scrollHeight + "px";
+        setFrameHeight(frame, target.scrollHeight);
     };
     var observer = new MutationObserver(callback);
     observer.observe(target, config);
+    setFrameHeight(frame, target.scrollHeight);
+}
+
+function setFrameHeight(frame, height) {
+    frame.style.height = height + "px";
 }
